829076caa9b92a7f80ed6e1bedc6a2556e581db7
FauzanTMR <134910253+FauzanTMR@users.noreply.github.com>
2025-02-02 00:36:20 -0800

    Add files via upload

---
+++ R.java
@@ -0,0 +1,124 @@
+package com.darskhandev.nassdek;
+
+public final class R {
+
+    public static final class color {
+        public static int accent_color = 2131034137;
+        public static int black = 2131034146;
+        public static int primary_color = 2131034846;
+        public static int primary_dark_color = 2131034847;
+        public static int primary_light_color = 2131034850;
+        public static int primary_text_color = 2131034853;
+        public static int secondary_text_color = 2131034860;
+        public static int white = 2131034873;
+
+        private color() {
+        }
+    }
+
+    public static final class drawable {
+        public static int baseline_home_24 = 2131165306;
+        public static int baseline_image_24 = 2131165307;
+        public static int baseline_image_search_24 = 2131165308;
+        public static int baseline_info_24 = 2131165309;
+        public static int bgpadang = 2131165310;
+        public static int dashed_border = 2131165338;
+        public static int ic_launcher_background = 2131165356;
+        public static int ic_launcher_foreground = 2131165357;
+        public static int iklan = 2131165366;
+        public static int logopadang = 2131165367;
+        public static int rounded_border = 2131165440;
+        public static int rounded_tentang = 2131165441;
+
+        private drawable() {
+        }
+    }
+
+    public static final class font {
+        public static int bold = 2131230720;
+        public static int book = 2131230721;
+        public static int light = 2131230722;
+        public static int meduim = 2131230723;
+
+        private font() {
+        }
+    }
+
+    public static final class id {
+        public static int aboutFragment = 2131296270;
+        public static int bottomNavigationView = 2131296354;
+        public static int btnDetect = 2131296359;
+        public static int classfierFragment = 2131296377;
+        public static int homeFragment = 2131296474;
+        public static int imagePicker = 2131296484;
+        public static int imageView = 2131296485;
+        public static int imgPreview = 2131296486;
+        public static int layoutResult = 2131296499;
+        public static int main = 2131296512;
+        public static int main_navigation = 2131296513;
+        public static int nav_host_fragment = 2131296574;
+        public static int txtResult = 2131296752;
+
+        private id() {
+        }
+    }
+
+    public static final class layout {
+        public static int activity_main = 2131492892;
+        public static int fragment_about = 2131492909;
+        public static int fragment_classfier = 2131492910;
+        public static int fragment_home = 2131492911;
+
+        private layout() {
+        }
+    }
+
+    public static final class menu {
+        public static int bottom_nav_menu = 2131623936;
+
+        private menu() {
+        }
+    }
+
+    public static final class mipmap {
+        public static int ic_launcher = 2131689472;
+        public static int ic_launcher_round = 2131689473;
+
+        private mipmap() {
+        }
+    }
+
+    public static final class navigation {
+        public static int main_navigation = 2131755008;
+
+        private navigation() {
+        }
+    }
+
+    public static final class string {
+        public static int app_name = 2131886108;
+        public static int hello_blank_fragment = 2131886151;
+
+        private string() {
+        }
+    }
+
+    public static final class style {
+        public static int Base_Theme_Nassdek = 2131951736;
+        public static int Theme_Nassdek = 2131952242;
+
+        private style() {
+        }
+    }
+
+    public static final class xml {
+        public static int backup_rules = 2132082688;
+        public static int data_extraction_rules = 2132082689;
+
+        private xml() {
+        }
+    }
+
+    private R() {
+    }
+}
